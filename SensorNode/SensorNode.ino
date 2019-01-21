5b62e7a579832aded80290259b53e73846d00cef
npyalex <np.alexander@gmail.com>
2019-01-21 10:38:03 -0500

    Calibration
    The photosensor will calibrate in the first 5 seconds it is powered. Cover and uncover it for best results.

--- SensorNode/SensorNode.ino
+++ SensorNode/SensorNode.ino
@@ -1,16 +1,31 @@
 int radioID = 15; // put your radio number here
+int sensorMin = 1023; //minimum photoresistor sensor value
+int sensorMax = 0; //maximum sensor value
+const int sensorPin = A0;
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   Serial1.begin(9600);
+  // calibrate the light sensor during the first five seconds
+  while(millis() < 5000) {
+    int calibrateValue = analogRead(sensorPin);
+    // record max value
+    if (calibrateValue > sensorMax) {
+      sensorMax = calibrateValue;
+    }
+    // record min value
+    if (calibrateValue < sensorMin) {
+      sensorMin = calibrateValue;
+    }
+  }
 }
 
 void loop() {
   // read the input on analog pin 0:
-  int pinValue = analogRead(A0); 
+  int pinValue = analogRead(sensorPin); 
   //map the data so it can be turned into a colour by Processing:
-  int sensorValue = map(pinValue, 100, 1000, 0, 255); 
+  int sensorValue = map(pinValue, sensorMin, sensorMax, 0, 255); 
   Serial1.print(radioID);
   Serial.print(radioID);
   Serial1.print(",");
